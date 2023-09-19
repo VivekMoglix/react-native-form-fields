66df55547b93f2eace2f3c6946253edf653ed6b2
VivekMoglix <vivek@moglix.com>
2023-09-19 14:25:25 +0530

    WIP: fix for default button design

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import Radio from "./components/Radio";
 import type { SwitchProps } from "./components/Switch";
 import Switch from "./components/Switch";
 
+export * from "./themes/colors";
+export * from "./themes/dimensions";
+
 export {
   TextInput,
   TextInputProps,
